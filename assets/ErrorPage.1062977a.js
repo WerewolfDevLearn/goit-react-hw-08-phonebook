f07b103b9332e57f0fd42ba5f9e4a739683e9087
WerewolfDevLearn <WerewolfDevLearn@users.noreply.github.com>
2023-06-07 10:08:02 +0000

    deploy: 67257824ebfa1122d03a11403193d29c9d376ac7

--- assets/ErrorPage.1062977a.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.b91b8df7.js";function o(){return r("p",{children:"Sorry. There is no such page"})}export{o as default};
---
+++ assets/HomePage.eb47ea7a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.17403dbb.js";function r(){return e("h1",{children:"Hello World"})}export{r as default};
